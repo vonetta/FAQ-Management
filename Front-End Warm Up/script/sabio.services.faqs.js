ef9e93e92fae11efa326d7c0332c57445b0261dd
Vonetta <vonettastevenson@gmail.com>
2017-12-12 21:40:18 -0800

    Add project files.

---
+++ Front-End Warm Up/script/sabio.services.faqs.js
@@ -0,0 +1,47 @@
+﻿var userUrl = "https://pacoima-ypi.azurewebsites.net/api/users/";
+var url = "https://pacoima-ypi.azurewebsites.net/api/";
+
+sabio.page.newFaq = function (data, onSuccess, onError) {
+    var settings = {
+        cache: false,
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        success: onSuccess,
+        error: onError,
+        xhrFields: {
+            withCredentials: true
+        }
+    };
+    $.ajax(url + "faqs", settings);
+}
+
+sabio.page.editFaq = function (data, id, onSuccess, onError) {
+    var settings = {
+        cache: false,
+        type: "PUT",
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        success: onSuccess,
+        error: onError,
+        xhrFields: {
+            withCredentials: true
+        }
+    };
+    $.ajax(url + "faqs/" + id, settings);
+}
+
+sabio.page.getFaq = function (data, id, onSuccess, onError) {
+    var settings = {
+        cache: false,
+        type: "GET",
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        success: onSuccess,
+        error: onError,
+        xhrFields: {
+            withCredentials: true
+        }
+    };
+    $.ajax(url + "faqs/" + id, settings);
+}
---
+++ Front-End Warm Up/script/sabio.services.users.js
@@ -0,0 +1,74 @@
+﻿var userUrl = "https://pacoima-ypi.azurewebsites.net/api/users/";
+var url = "https://pacoima-ypi.azurewebsites.net/api/";
+
+ sabio.page.register = function (data, onSuccess, onError) {
+	var settings = {
+		cache: false,
+		contentType: "application/json",
+		data: JSON.stringify(data),
+		type: "POST",
+		xhrFields: {
+			withCredentials: true
+		},
+		success: onSuccess,
+		error: onError
+	};
+	$.ajax(userUrl + "register/employer", settings);
+ }
+
+ sabio.page.login = function (data, onSuccess, onError) {
+ 	var settings = {
+ 		cache: false,
+ 		contentType: "application/json",
+ 		data: JSON.stringify(data),
+ 		type: "POST",
+ 		xhrFields: {
+ 			withCredentials: true
+ 		},
+ 		success: onSuccess,
+ 		error: onError
+ 	};
+ 	$.ajax(userUrl + "login/force", settings);
+ }
+
+ sabio.page.logout = function (onSuccess, onError) {
+ 	var settings = {
+ 		cache: false,
+ 		type: "GET",
+ 		xhrFields: {
+ 			withCredentials: true
+ 		},
+ 		success: onSuccess,
+ 		error: onError
+ 	};
+ 	$.ajax(userUrl + "logout", settings);
+ }
+
+ sabio.page.getUser = function (onSuccess, onError) {
+ 	var settings = {
+ 		cache: false,
+ 		type: "GET",
+ 		xhrFields: {
+ 			withCredentials: true
+ 		},
+ 		success: onSuccess,
+ 		error: onError
+ 	};
+ 	$.ajax(url + "people/currentuser", settings);
+ }
+
+ sabio.page.getCategories = function (onSuccess, onError) {
+     var settings = {
+         cache: false,
+         type: "GET",
+         success: onSuccess,
+         error: onError,
+         xhrFields: {
+             withCredentials: true
+         }
+     };
+     $.ajax(url + "faqcategories", settings);
+ }
+
+
+
