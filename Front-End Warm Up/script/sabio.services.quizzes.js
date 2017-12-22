3e7551cb4e3bbeee983a5e3eeb75c705c3604f1d
Vonetta <vonettastevenson@gmail.com>
2017-12-22 00:37:03 -0800

    more es6 conversions and delete functionality

--- Front-End Warm Up/script/sabio.services.quizzes.js
+++ Front-End Warm Up/script/sabio.services.quizzes.js
@@ -60,12 +60,10 @@ sabio.services.postQuizQuestion = (data, onSuccess, onError) => {
     $.ajax(url + "quizzes/questions", settings);
 }
 
-sabio.services.deleteQuizQuestion = (data, id, onSuccess, onError) => {
+sabio.services.deleteQuizQuestion = (id, onSuccess, onError) => {
     var settings = {
         cache: false,
         type: "DELETE",
-        contentType: "application/Json",
-        data: JSON.stringify(data),
         success: onSuccess,
         error: onError,
         xhrFields: {
@@ -73,4 +71,32 @@ sabio.services.deleteQuizQuestion = (data, id, onSuccess, onError) => {
         }
     };
         $.ajax(url + "/quizzes/questions/" + id ,settings);
+}
+
+sabio.services.updateQuizQuestion = (data, onSuccess, onError) => {
+    var settings = {
+        cache: false,
+        type: "PUT",
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        success: onSuccess,
+        error: onError,
+        xhrFields: {
+            withCredentials: true
+        }
+    };
+    $.ajax(url + "quizzes/questions/" + id, settings);
+}
+
+sabio.services.getQuizQuestion = (onSuccess, onError) => {
+    var settings = {
+        cache: false,
+        type: "GET",
+        success: onSuccess,
+        error: onError,
+        xhrFields: {
+            withCredentials: true
+        }
+    };
+    $.ajax(url, settings);
 }
